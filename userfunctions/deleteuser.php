e06251e95549ecc615f3b938a41e28de8e7977ba
JakeLardinois <Jake.Lardinois@Gmail.com>
2013-12-28 09:11:15 -0600

    Implemented delete user and update user functionality

--- userfunctions/deleteuser.php
+++ userfunctions/deleteuser.php
@@ -1,10 +1,19 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+	require_once '../infrastructure/dbconfig.php';
 
-<body>
-</body>
-</html>
+	//This prevents an unauthenticated user from utilizing the buildeditablecalendar.js file to delete events; it is a failsafe measure since the first line against unauthorized
+	//access is loading buildreadonlycalendar.js for unauthenticated users.
+	if(empty($_SESSION['LoggedIn']) && empty($_SESSION['Username'])){exit();}
+	/* Values received via ajax */
+	$id = $_POST['id'];
+
+	// connection to the database
+	try {
+		$bdd = new PDO('mysql:host='.mysql_hostname.';dbname='.mysql_dbname, mysql_username, mysql_password);
+	} catch(Exception $e) { exit('Unable to connect to database.'); }
+
+	// delete the records
+	$sql = "DELETE from users WHERE userid=".$id;
+	$q = $bdd->prepare($sql);
+	$q->execute();
+?>
--- userfunctions/updateuser.php
+++ userfunctions/updateuser.php
@@ -6,7 +6,19 @@ $value = $_POST['value'];
 $rowId = $_POST['rowId'];
 $columnPosition = $_POST['columnPosition'];
 $columnId = $_POST['columnId'];
-$columnName = $_POST['columnName'];
+$columnName = str_replace(' ', '',$_POST['columnName']);
 
+// connection to the database
+try {
+	$bdd = new PDO('mysql:host='.mysql_hostname.';dbname='.mysql_dbname, mysql_username, mysql_password);
+} catch(Exception $e) {
+	exit('Unable to connect to database.');
+}
+// update the records
+$sql = "UPDATE users SET ".$columnName."=? WHERE userid=".$id;
+$q = $bdd->prepare($sql);
+$q->execute(array($value));
+
+//echo json_encode(array('Success' => true));//sends json response back to ajax telling it it was successful...
 echo $value;
 ?>
