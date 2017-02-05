124acde7cffdd0750d151b02bd8eb2cc4201b2f2
JakeLardinois <jake.lardinois@gmail.com>
2017-02-05 08:02:39 -0600

    Created wp-config

--- DoorCountyAA/wp-config.php
+++ DoorCountyAA/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'dcaa');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'webAppUser');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'wh0r353');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
