e00a91a739f30c70867259c45b3210621fc7f884
JakeLardinois <jake.lardinois@gmail.com>
2017-03-15 06:40:41 -0500

    Added my first plugin

---
+++ DoorCountyAA/wp-content/plugins/JakesHelloWorld/jakes_hello_world.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Jake's Hello World!
+Version: 0.1
+Description: My First Wordpress Plugin
+Author: Jake Lardinois
+Author URI: www.IntegralSWSolutions.com
+Plugin URI: www.IntegralSWSolutions.com
+Text Domain: Hello!
+Domain Path: /languages
+ */
+
+add_shortcode('JakesHelloWorldShort', 'process_HelloWorld');
+function process_HelloWorld() {
+    $returnHTML = file_get_contents('jakes_hello_world.htm', true);
+    $returnHTML = str_replace("##jake##", "Jake", $returnHTML);
+
+    return $returnHTML;
+}
