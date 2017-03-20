8762e87faf2f8231f15ad60fb6e21214f5ad6186
JakeLardinois <jake.lardinois@gmail.com>
2017-03-20 07:27:26 -0500

    Added Content and Styled

--- DoorCountyAA/wp-content/themes/DoorCountyAA/footer.php
+++ DoorCountyAA/wp-content/themes/DoorCountyAA/footer.php
@@ -6,6 +6,16 @@
 					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
 					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
 				</p>
+                <p>
+                    AA World Services has neither reviewed
+                    nor endorsed this page. It is provided by the Door and Kewaunee county
+                    area Intergroup, and does not represent Alcoholics Anonymous as a whole.
+                </p>
+                <p>
+                    Alcoholics Anonymous&reg;, A.A.&reg;, and The
+                    Big Book&reg; are registered trademarks of Alcoholics Anonymous World Services,
+                    Inc.
+                </p>
 				<!-- /copyright -->
 
 			</footer>
